869a7d3ac7af263522a404e0121a0f9fc2c7e619
Carlton Bryan <carlton@w4jcb.com>
2022-01-21 14:11:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ homedir=/home/${USER} #/home/user_name
 dotfiledir=${DIR}
 
 # list of files/folders to symlink in ${homedir}
-files="vimrc bash_aliases"
+files="bash_aliases"
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
@@ -30,32 +30,6 @@ for file in ${files}; do
     ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
 done
     
-# For my customized vim file    
-# list of files/folders to symlink in ${homedir}/.vim
-files2="custom.vim"
-
-# custom vim directory
-customdir=${homedir}/.vim
-
-# create symlinks (will overwrite old dotfiles)
-for file in ${files2}; do
-    echo "Creating symlink to $file in home/.vim directory."
-    ln -sf ${dotfiledir}/${file} ${customdir}/${file}
-done
-
-# For my customized Neovim file    
-# list of files/folders to symlink in ${homedir}/.config/nvim
-files3="init.vim"
-
-# custom vim directory
-customdir=${homedir}/.config/nvim
-
-# create symlinks (will overwrite old dotfiles)
-for file in ${files3}; do
-    echo "Creating symlink to $file in home/.vim directory."
-    ln -sf ${dotfiledir}/${file} ${customdir}/${file}
-done
-
 # Install packages I can't live without.
 xargs -a packages.txt sudo apt-get install
 sudo pip install -r requirements.txt
